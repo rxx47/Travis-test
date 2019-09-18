3d6bc8eeb0612f74253bd5a2f974de2d9da13827
Rui Xin <rui.xin@duke.edu>
2019-09-18 12:41:10 -0400

    test travis

---
+++ test_calculate.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.ark.parametrize("a,b, expected"),[
+                            (2, 3, 5),
+                            (3, 7, 10),
+                            (-2, 5, 3),
+                            (0.1,0.2,0.3)
+                            ])
+
+def test_add(a, b, expected)
+    from calculate import add
+    result = add(2,3)
+    expected = 5
+    assert result == pytest.approx(expected)
