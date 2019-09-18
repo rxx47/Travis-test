38aeb55cfd434cf477fcf4d1efb1d09059c03444
Rui Xin <rui.xin@duke.edu>
2019-09-18 12:49:25 -0400

    fixed the test file

--- test_calculate.py
+++ test_calculate.py
@@ -1,13 +1,13 @@
 import pytest
 
-@pytest.ark.parametrize("a,b, expected"),[
+@pytest.mark.parametrize("a,b, expected",[
                             (2, 3, 5),
                             (3, 7, 10),
                             (-2, 5, 3),
                             (0.1,0.2,0.3)
                             ])
-
-def test_add(a, b, expected)
+                            
+def test_add(a, b, expected):
     from calculate import add
     result = add(2,3)
     expected = 5
